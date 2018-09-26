2da4bbf4050f70c891963d64c860ea39b9ee8e09
user-yongheng <yongheng@email.com>
2018-09-26 10:51:15 +0800

    add '[' and ']' around time in log message

--- build/lib/hutils/__init__.py
+++ build/lib/hutils/__init__.py
@@ -5,6 +5,6 @@ def log(msg, logfile):
     curtime = str(datetime.datetime.now())
     with open(logfile, "a") as f:    
         f.write("\n\n")
-        f.write("time: " + curtime)
+        f.write("[time: {}]".format(curtime))
         f.write("\n")
         f.write("message: " + msg)
