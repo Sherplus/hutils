c4d3a550e52c3c17060807c735c6c240f481679a
user-yongheng <yongheng@email.com>
2018-10-24 16:15:36 +0800

    add hutils.get_research_dataset_dir()

--- hutils/__init__.py
+++ hutils/__init__.py
@@ -17,3 +17,12 @@ def log(msg):
         f.write("[time: {}]".format(curtime))
         f.write("\n")
         f.write("message: " + msg)
+
+def get_research_dataset_dir():
+    cur_sys = platform.system()   # cur_sys = ['Windows', 'Linux', etc.]
+    if cur_sys == 'Windows':
+        return 'D:/research/dataset'
+    elif cur_sys == 'Linux':
+        return '/home/mira/research/dataset'
+    else:
+        raise RuntimeError('unknown system')
